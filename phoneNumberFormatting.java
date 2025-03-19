1ddf2d623bb5a581ecfebcea1399f60a39acd467
Franco Bernardele <francobernar@gmail.com>
2025-03-19 09:29:28 -0300

    phoneNumberFormatting challenge 21/2

---
+++ phoneNumberFormatting.java
@@ -0,0 +1,17 @@
+static String devolverNro(int[] numero)
+    {
+        String numeroReturn = "(";
+        for (int i = 0; i < numero.length; i++) {
+            if (i == 3)
+            {
+                numeroReturn += ") ";
+            }
+            if (i==6)
+            {
+                numeroReturn += "-";
+            }
+            numeroReturn += numero[i];
+
+        }
+        return numeroReturn;
+    }
