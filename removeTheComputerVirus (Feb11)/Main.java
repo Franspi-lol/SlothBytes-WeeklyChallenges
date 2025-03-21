3b8765967fd50e6e76c6bd21da45ca153913a8da
Franco Bernardele <francobernar@gmail.com>
2025-03-21 10:01:35 -0300

    Doc

--- removeTheComputerVirus (Feb11)/Main.java
+++ removeTheComputerVirus (Feb11)/Main.java
@@ -1,24 +1,36 @@
-//February 11th. Franco Bernardele
+public class VirusRemover {
 
-public static void main (){
-    System.out.println(remove_virus("PC Files: spotifysetup.exe, virus.exe, dog.jpg, lethalmalware.exe, antivirus.exe"));
-}
-
-public static String remove_virus(String input) {
-    String[] words = input.split(" ");
-    for (int i = 0; i < words.length; i++) {
-        if (words[i].contains("virus")){
-            if ((!words[i].contains("anti"))&&(!words[i].contains("not"))){
-                words[i] = "";
-            }
-        }
-        if (words[i].contains("malware")){
-            if ((!words[i].contains("anti"))&&(!words[i].contains("not"))){
-                words[i] = "";
-            }
-        }
+    /**
+     * The main method demonstrates the remove_virus method with a sample input string.
+     *
+     * @author Franco Bernardele
+     */
+    public static void main(String[] args) {
+        System.out.println(remove_virus("PC Files: spotifysetup.exe, virus.exe, dog.jpg, lethalmalware.exe, antivirus.exe"));
     }
-    return String.join(" ", words);
-}
 
+    /**
+     * Removes file names containing "virus" or "malware" from an input string, unless they also contain "anti" or "not".
+     *
+     * @param input The input string containing a list of file names separated by spaces.
+     * @return A string with the virus and malware file names removed.
+     * @author Franco Bernardele
+     */
+    public static String remove_virus(String input) {
+        String[] words = input.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].contains("virus")) {
+                if ((!words[i].contains("anti")) && (!words[i].contains("not"))) {
+                    words[i] = "";
+                }
+            }
+            if (words[i].contains("malware")) {
+                if ((!words[i].contains("anti")) && (!words[i].contains("not"))) {
+                    words[i] = "";
+                }
+            }
+        }
+        return String.join(" ", words);
+    }
+}
 
