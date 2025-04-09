7caa052ff7a45ab40649c76b6f17b3d456db55f0
Franco Bernardele <francobernar@gmail.com>
2025-04-09 11:25:11 -0300

    Add StickyKeysTyping functionality and initial implementation of isLongPressed method

---
+++ StickyKeysTyping/StickyKeysTyping.java
@@ -0,0 +1,13 @@
+package StickyKeysTyping;
+
+public class StickyKeysTyping {
+    public static void main(String[] args) {
+        System.out.println(isLongPressed("alex", "aaleex"));
+    }
+
+    public static boolean isLongPressed(String original, String typed)
+    {
+
+        return true;
+    }
+}
